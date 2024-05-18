324bcaf41cc35913742c72727af063b99a782bce
Kawsika123 <164658751+Kawsika123@users.noreply.github.com>
2024-05-19 00:21:11 +0530

    this one try it new

---
+++ src/databaseConnection.java
@@ -0,0 +1,27 @@
+/*
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Wethum Kawsika
+ */
+/*
+public class databaseConnection {
+    static Connection connection(){
+        try{
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection con=(Connection) DriverManager.getConnection("jdbc:mysql://172.20.10.2:3306/schoolmanagement2","root","");
+        return con;
+    }
+        catch(Exception e){
+            System.out.println(e);
+            return null;
+        }
+    }
+}
+*/
